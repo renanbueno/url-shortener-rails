293aec8b8022f282686a988bbcc1aa4b12e6fd95
Renan Bueno Angioletto <renan.angioletto@gmail.com>
2024-03-27 17:31:07 -0300

    created auto generated slug method

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,7 +5,23 @@ class Link < ApplicationRecord
   validates_length_of :url, within: 3..255, on: :create, message: "too long"
   validates_length_of :slug, within: 3..255, on: :create, message: "too long"
 
+  before_validation :generate_slug
+  def generate_slug
+    self.slug = SecureRandom.uuid[0..5] if self.slug.nil? || self.slug.empty?
+    true
+  end
+
   def short
     Rails.application.routes.url_helpers.short_url(slug: self.slug)
+  end
+
+  def self.shorten(url, slug = '')
+    link = Link.where(url: url, slug: slug).first
+    return link.short if link  
+    
+    link = Link.new(url: url, slug: slug)
+    return link.short if link.save
+    
+    Link.shorten(url, slug + SecureRandom.uuid[0..2])
   end
 end
