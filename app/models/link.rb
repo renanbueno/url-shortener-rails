8b1f11b6ad69e869b7bf44bdeff7df34b5a4e8f8
Renan Bueno Angioletto <renan.angioletto@gmail.com>
2024-03-27 17:22:19 -0300

    added dafault host and slug method of link

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,4 +4,8 @@ class Link < ApplicationRecord
   validates_uniqueness_of :slug
   validates_length_of :url, within: 3..255, on: :create, message: "too long"
   validates_length_of :slug, within: 3..255, on: :create, message: "too long"
+
+  def short
+    Rails.application.routes.url_helpers.short_url(slug: self.slug)
+  end
 end
