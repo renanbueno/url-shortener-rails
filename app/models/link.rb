e0d5fbcb76331fe74e2a106985aa7b53075553c0
Renan Bueno Angioletto <renan.angioletto@gmail.com>
2024-03-26 23:15:04 -0300

    added link model

---
+++ app/models/link.rb
@@ -0,0 +1,7 @@
+class Link < ApplicationRecord
+  validates_presence_of :url
+  validates :url, format: URI::regexp(%w[http https])
+  validates_uniqueness_of :slug
+  validates_length_of :url, within: 3..255, on: :create, message: "too long"
+  validates_length_of :slug, within: 3..255, on: :create, message: "too long"
+end
--- db/migrate/20240327020654_create_links.rb
+++ db/migrate/20240327020654_create_links.rb
@@ -1,7 +1,9 @@
 class CreateLinks < ActiveRecord::Migration[7.1]
   def change
     create_table :links do |t|
-
+      t.string :url
+      t.string :slug
+      t.integer :clicked, default: 0
       t.timestamps
     end
   end
